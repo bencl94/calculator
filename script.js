762a548b0c27deb706e3405b93d3fac3bc26fcf4
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-04-15 21:48:37 +0200

    Add operator function and globals

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let operand1, operand2, operator;
+
 const add = (a, b) => {
     return a + b;
 }
@@ -14,4 +16,17 @@ const divide = (a, b) => {
     if (b === 0) throw Error('Zero division not possible');
 
     return a / b;
+}
+
+const operate = (op1, sign, op2) => {
+    switch (sign) {
+        case '+':
+            return add(op1, op2);
+        case '-':
+            return subtract(op1, op2);
+        case '*':
+            return multiply(op1, op2);
+        case '/':
+            return divide(op1, op2);
+    }
 }
